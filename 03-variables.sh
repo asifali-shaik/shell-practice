9718ff7ebc8d1598be862eb5197e0a88390b8fb1
asif <asifalishaik415@gmail.com>
2026-01-29 03:51:28 -0800

    changed

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,3 +4,17 @@ ICECREAM="black current"
 
 echo " my name is $USERNAME and my favourite ice cream is $ICECREAM"
 echo " thanks for giving me this $ICECREAM"
+
+
+
+MOM="PARVEEN"
+DISH="chicken soup"
+
+echo " my mom name is $MOM and her favourtie dish is $DISH"
+
+ME=asif 
+MOM=parveen 
+
+MAKING="can you make me some chicken soup"
+
+echo "my $MOM said to me hey $ME can you make me some" 
