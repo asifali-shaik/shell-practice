bbcce32b07b42fa98753c4e90efbea4b8d2e6ae3
asif <asifalishaik415@gmail.com>
2026-01-29 22:14:55 -0800

    conv

--- 04-readonly.sh
+++ 04-readonly.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-person1=$1
-person2=$2
+echo "please enter your username: "
+read name
 
-
-echo " $person1 can you give me this book ."
-echo " $perosn2 yeah for sure "
-echo " $person1 ok thanks "
-echo " $person2 welcome dosth "
+echo username is $name
