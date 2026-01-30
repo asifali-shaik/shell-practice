abb7a0d41fa48d3108bdd451028435dd65399cbf
asif <asifalishaik415@gmail.com>
2026-01-30 05:19:22 -0800

    conv

---
+++ 08-cond.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#conditions in shell; two types of conditions in shell if and else so now lets see if&else syntax 
+#if [expressions]; then
+#   code goes here
+#else 
+#   code goes here  
+#fi
+
+MONEY=$1
+
+if [ $MONEY -gt 100 ]; then
+    echo " you can buy me a pizza if you want to"
+fi    
