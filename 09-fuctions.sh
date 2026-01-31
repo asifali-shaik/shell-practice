ab4714fed7c5e56456045b5d19157510a6dbd3c6
asif <asifalishaik415@gmail.com>
2026-01-30 21:31:14 -0800

    conv

---
+++ 09-fuctions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]; then
+    echo " please run this with root user"
+    exit 1
+fi
+
+STATUSHECK(){
+    if [ $1 -ne 0 ]; then
+        echo " $2 ... failure"
+        exit 1
+    else 
+        echo "$2 ...success"
+    fi    
+} 
+
+
+
+dnf install nginx -y
+STATUSCHECK $? "installing nginx"
+
+dnf install mysql -y
+STATUSCHECK $? "installing mysql"
+
+dnf install nodejs -y
+STATUSCHECK $? "installing nodejs"
