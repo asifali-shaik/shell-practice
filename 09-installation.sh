574c97ce43f1924b06efd0562b752df03ad3bfeb
asif <asifalishaik415@gmail.com>
2026-01-30 19:10:26 -0800

    conv

--- 09-installation.sh
+++ 09-installation.sh
@@ -9,3 +9,10 @@ fi
 
 echo "installing nginx"
 dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "installing nginx is .... failure"
+    exit 1
+else
+    echo "installing nginx is ...success"
+fi
